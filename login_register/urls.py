602edc21d9ce08b44cb4d12389f859cce72e69f4
John Gualteros <gualterosjohn40@gmail.com>
2022-06-20 15:21:17 -0500

    I add the funtionality with register new users

--- login_register/urls.py
+++ login_register/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register,login
+from .views import register,login,create_user
 from django.conf.urls import handler404
 from foreach.views import error404
 urlpatterns=[
     path('register/',register, name='register'),
+    path('register/create_user', create_user, name='create_user'),
     path('login/',login,name="login")
 ]
 handler404 = error404
--- login_register/views.py
+++ login_register/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import User
 # Create your views here.
 def register(request):
     return render(request, 'register.html')
+def create_user(request):
+    user = User(userName=request.POST.get('user'),password=request.POST.get('password'))
+    user.save()
+    print(request.POST)
+    return redirect('/foreach/')
 def login(request):
     return render(request, 'login.html')
